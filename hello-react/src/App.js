7be545a5378586f2ba4e6c0509c3ecd70d6304f6
jwlee <jwlee@utopsoft.co.kr>
2020-12-01 18:26:38 +0900

    1201 backup

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,17 +1,21 @@
 import React, {Component} from 'react';
-import MyComponent from './MyComponent'
-import MyClassComponent from './MyClassComponent'
-import Counter from './Counter'
-import Say from './Say'
-import EventPracticeClass from './EventPracticeClass'
-import EventPractice from './EventPractice'
-import ValidationSample from './ValidationSample'
-import ScrollBox from './ScrollBox'
+import MyComponent from './components/MyComponent'
+import MyClassComponent from './components/MyClassComponent'
+import Counter from './components/Counter'
+import Say from './components/Say'
+import EventPracticeClass from './components/EventPracticeClass'
+import EventPractice from './components/EventPractice'
+import ValidationSample from './components/ValidationSample'
+import ScrollBox from './components/ScrollBox'
+import IterationSample from './components/IterationSample'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <IterationSample />
+        <br/>
+        <br/>
         <ScrollBox ref={(ref) => this.scrollBox=ref}/>
         <button onClick={() => this.scrollBox.scrollToBottom()}>to bottom</button>
         <br/>
